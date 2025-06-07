40e91689ad35ad5fe3715dfecab9d44c68b744ca
Thibault Poiré <poire.thibault@gmail.com>
2025-06-07 17:25:19 +0200

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*', credentials: true });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
